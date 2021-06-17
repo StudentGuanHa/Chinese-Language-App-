5a3860cc20b54664c5550154312dc36ad3e57f32
StudentGuanHa <83789455+StudentGuanHa@users.noreply.github.com>
2021-06-17 14:46:59 +1000

    Add files via upload

---
+++ Chinese learning app/Chinese learning app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chinese learning app
+//
+//  Created by Harry Guan on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
